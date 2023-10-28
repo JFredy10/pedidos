f825a20996c76fe34f145158c9596dda96de70c4
Fredy <fjarava@utb.edu.co>
2023-10-28 13:30:27 -0500

    nueva actulizacion

--- main.py
+++ main.py
@@ -113,6 +113,7 @@ def delete_pedido(pedido_id: int):
 
     return {'mensaje': 'Pedido eliminado'}
 
+
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
