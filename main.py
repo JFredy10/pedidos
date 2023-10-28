7ab98100cc712ed43efc2cdc5a84b9374c0902cc
Fredy <fjarava@utb.edu.co>
2023-10-28 13:08:10 -0500

    actualizacion

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from sqlalchemy.testing import db
+import psycopg2
 import uvicorn
 
 app = FastAPI()
 
 # Configuración de la conexión a la base de datos
-import psycopg2
-
-# Parámetros de conexión
+# Se utiliza el formato correcto para la URL de conexión
 database_url = "postgresql://databasemenu_user:ZnoY5wh7SjJ3aybp42olfAeaR6xmzWWm@dpg-ckr9rehrfc9c73djbtu0-a/databasemenu"
 
 # Establecer la conexión a la base de datos
@@ -17,15 +15,8 @@ try:
     cursor = conn.cursor()
     print("Conexión a la base de datos PostgreSQL exitosa")
 
-    # Aquí puedes realizar consultas y operaciones en la base de datos utilizando el cursor
-
-    # No olvides cerrar la conexión cuando hayas terminado
-    cursor.close()
-    conn.close()
-
 except (Exception, psycopg2.Error) as error:
     print("Error al conectar a la base de datos PostgreSQL:", error)
-
 
 # Modelo de datos para el pedido
 class Pedido(BaseModel):
@@ -34,14 +25,12 @@ class Pedido(BaseModel):
     estado_pedido: str
     fecha_pedido: str
 
-
 # Ruta para obtener todos los pedidos
 @app.get('/pedidos')
 def get_pedidos():
     cursor.execute("SELECT * FROM pedido")
     pedidos = cursor.fetchall()
     return pedidos
-
 
 # Ruta para crear un nuevo pedido
 @app.post('/pedidos')
@@ -53,14 +42,14 @@ def create_pedido(pedido: Pedido):
 
     cursor.execute("INSERT INTO pedido (cliente, total, estado_pedido, fecha_pedido) VALUES (%s, %s, %s, %s)",
                    (cliente, total, estado_pedido, fecha_pedido))
-    db.commit()
+    conn.commit()  # Se cambia 'db.commit()' a 'conn.commit()'
 
     # Obtén el ID del pedido recién creado
     cursor.execute("SELECT lastval()")
     pedido_id = cursor.fetchone()[0]
+    conn.commit()  # Se agrega 'conn.commit()' para confirmar la transacción
 
     return {'mensaje': 'Pedido creado', 'pedido_id': pedido_id}
-
 
 # Ruta para obtener un pedido por ID
 @app.get('/pedidos/{pedido_id}')
@@ -70,7 +59,6 @@ def get_pedido(pedido_id: int):
     if pedido:
         return pedido
     raise HTTPException(status_code=404, detail='Pedido no encontrado')
-
 
 # Ruta para actualizar un pedido por ID
 @app.put('/pedidos/{pedido_id}')
@@ -82,16 +70,15 @@ def update_pedido(pedido_id: int, pedido: Pedido):
 
     cursor.execute("UPDATE pedido SET cliente = %s, total = %s, estado_pedido = %s, fecha_pedido = %s WHERE id = %s",
                    (cliente, total, estado_pedido, fecha_pedido, pedido_id))
-    db.commit()
+    conn.commit()  # Se cambia 'db.commit()' a 'conn.commit()'
     return {'mensaje': 'Pedido actualizado'}
-
 
 # Ruta para eliminar un pedido por ID
 @app.delete('/pedidos/{pedido_id}')
 def delete_pedido(pedido_id: int):
     cursor.execute("DELETE FROM pedido WHERE id = %s", (pedido_id,))
-    db.commit()
+    conn.commit()  # Se cambia 'db.commit()' a 'conn.commit()'
     return {'mensaje': 'Pedido eliminado'}
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
